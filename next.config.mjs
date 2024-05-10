2468bc1c19fac1205993521e8a6057fcb5ab0a94
dedmondson <dedmondson@truecar.com>
2024-05-10 13:30:20 -0700

    Show caching problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ["@/ui"],
+  },
+};
 
 export default nextConfig;
---
+++ ui/index.ts
@@ -0,0 +1,3 @@
+// Delete this import (no error will occur)
+// Rename Button.tsx (a stale error will occur)
+export { Button } from "./Button";
